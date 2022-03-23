cd16b79d5d4d228971baae050ef829c2a5e566a0
John Downie <johndownie@Johns-MacBook-Pro.local>
2022-03-23 11:18:20 +0000

    Implement basic leaflet map

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,24 @@
 <template>
+<div style="height: 100vh; width: 100vw;">
+    <l-map
+      v-model="zoom"
+      v-model:zoom="zoom"
+      :center="[47.41322, -1.219482]"
+      @move="log('move')"
+    >
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      ></l-tile-layer>
+
+      <l-marker :lat-lng="[50, 50]" draggable @moveend="log('moveend')">
+        <l-popup>
+          lol
+        </l-popup>
+      </l-marker>
+
+
+    </l-map>
+  </div>
   <div id="app">
     
     <h1>Spot Weather</h1>
@@ -11,11 +31,25 @@
 </template>
 
 <script>
+import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LPopup,
+} from "@vue-leaflet/vue-leaflet";
+import "leaflet/dist/leaflet.css";
 import axios from 'axios';
 export default {
   name: 'App',
+    components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+    LPopup
+  },
   data() {
     return {
+      zoom: 2,
       location: ''
     }
   },
@@ -31,7 +65,10 @@ export default {
       const forecast = await axios.get('https://api-metoffice.apiconnect.ibmcloud.com/metoffice/production/v0/forecasts/point/daily?excludeParameterMetadata=false&includeLocationName=true&latitude=50.388139&longitude=-4.137786', config);
       console.log(forecast);
       this.location = forecast.data.features[0].properties.location.name
-    }
+    },
+    log(a) {
+      console.log(a);
+    },
   }
 }
 </script>

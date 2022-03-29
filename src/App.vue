751be1068cea4d5e8f1463de9aea54047e3dfc89
John Downie <johndownie@Johns-MacBook-Pro.local>
2022-03-29 20:27:49 +0100

    Configure API environemnt variables

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,9 @@
 import LeafletMap from "./components/LeafletMap.vue";
 import axios from 'axios';
 import dayjs from 'dayjs';
+import { X_IBM_CLIENT_ID, X_IBM_CLIENT_SECRET } from './../apikey';
+
+
 
 export default {
   name: 'App',
@@ -50,8 +53,8 @@ export default {
       let config = {
         headers: {
           'Accept': 'application/json',
-          'X-IBM-Client-Id': '875b363a-9623-4a8a-9c68-d51802b5be65',
-          'X-IBM-Client-Secret': 'yR2hM6aN2lI3jO3eS2nL2qB8kK6oP8yC8qN6cA3nB4cY5uU0qO'
+          'X-IBM-Client-Id': X_IBM_CLIENT_ID,
+          'X-IBM-Client-Secret': X_IBM_CLIENT_SECRET
         }
       }
       const forecast = await axios.get('https://api-metoffice.apiconnect.ibmcloud.com/metoffice/production/v0/forecasts/point/daily?excludeParameterMetadata=false&includeLocationName=true&latitude=' + this.lat + '&longitude=' + this.lng, config);
